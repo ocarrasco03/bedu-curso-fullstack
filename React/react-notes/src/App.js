64aeda994e4129dcc521ddab7eba9f2a7b70a9aa
Oscar <anubis.carrasco03@gmail.com>
2019-06-22 14:33:17 -0500

    Leyendo props de la URL

--- React/react-notes/src/App.js
+++ React/react-notes/src/App.js
@@ -1,39 +1,42 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
+import { Grid, Fab } from '@material-ui/core';
+import { Link, Route } from 'react-router-dom';
+import AddIcon from '@material-ui/icons/Add';
 import NotesForm from './NotesForm';
 import NotesList from './NotesList';
-import { Link } from 'react-router-dom';
+import Home from './Home';
+import Note from './Note';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      description: '',
-      notes: [],
-      title: '',
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			description: '',
+			notes: [],
+			title: ''
+		};
+	}
 
-  updateState = field => event => {
-    this.setState({
-      [field]: event.target.value
-    });
-  }
+	updateState = (field) => (event) => {
+		this.setState({
+			[field]: event.target.value
+		});
+	};
 
-  saveNote = () => {
+	saveNote = () => {
 		if (this.state.title && this.state.description) {
 			this.setState({
-        description: '',
-				notes: [ 
-          ...this.state.notes, 
-          { 
-            id: Date.now(),
-            title: this.state.title, 
-            description: this.state.description 
-          } 
-        ],
-        title: ''
+				description: '',
+				notes: [
+					...this.state.notes,
+					{
+						id: Date.now(),
+						title: this.state.title,
+						description: this.state.description
+					}
+				],
+				title: ''
 			});
 		}
 	};
@@ -43,31 +46,40 @@ class App extends React.Component {
 			notes: this.state.notes.filter((_, i) => i !== index)
 		});
 	};
-  
-  render () {
-    console.log(this.state);
-    return ( 
-      <React.Fragment>
-        <Typography align="center" variant="h2" gutterBottom>My Notes</Typography>
-        <Grid container justify="center" spacing={2}>
-          <Grid item xs={8}>
-            <NotesForm 
-              title={this.state.title}
-              description={this.state.description}
-              updateState={this.updateState}
-              saveNote={this.saveNote}
-            />
-          </Grid>
-          <Grid item xs={8}>
-            <NotesList 
-              notes={this.state.notes}
-              deleteNote={this.deleteNote}
-              />
-          </Grid>
-        </Grid>
-      </React.Fragment>
-    );
-  }
+
+	render() {
+		console.log(this.state);
+		return (
+			<React.Fragment>
+				<Typography align="center" variant="h2" gutterBottom>
+					My Notes
+				</Typography>
+				<Grid container justify="center" spacing={2}>
+					<Grid item xs={8}>
+						<Route exact path="/" component={Home} />
+						<Route
+							path="/new"
+							render={() => (
+								<NotesForm
+									title={this.state.title}
+									description={this.state.description}
+									updateState={this.updateState}
+									saveNote={this.saveNote}
+								/>
+							)}
+						/>
+            <Route path='/view/:id' render={props => <Note {...props} notes={this.state.notes} />} />
+					</Grid>
+					<Grid item xs={8}>
+            <NotesList notes={this.state.notes} deleteNote={this.deleteNote} />
+					</Grid>
+				</Grid>
+				<Fab color="primary" component={Link} to={'/new'} size="small">
+					<AddIcon />
+				</Fab>
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
---
+++ React/react-notes/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+const Home = () => {
+    return(
+        <Typography align="center" variant="h3" gutterBottom>
+            Welcome Back!
+        </Typography>
+    )
+}
+
+export default Home;
