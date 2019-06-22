0c5a73922f4661688833af7d692b6b62d82d1d7a
Oscar <anubis.carrasco03@gmail.com>
2019-06-22 09:49:52 -0500

    Creacion de nuevo proyecto

---
+++ React/react-notes/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Fab from '@material-ui/core/Fab';
+import Icon from '@material-ui/core/Icon';
+import './App.css';
+import { TextField } from '@material-ui/core';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      description: '',
+      notes: '',
+      title: '',
+    }
+  }
+  render () {
+    return ( 
+      <React.Fragment>
+        <Typography align="center" variant="h2" gutterBottom>My Notes</Typography>
+        <Grid container justify="center" spacing={2}>
+          <Grid item xs={8}>
+            <Grid item xs={12}>
+              <TextField
+                type="text"
+                placeholder="Title for this note..."
+                margin="normal"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                placeholder="Start taking notes..."
+                margin="normal"
+                multiline
+                rows="4"
+                fullWidth
+              />
+            </Grid>
+            <Fab color="secondary">
+              <Icon>edit_icon</Icon>
+            </Fab>
+          </Grid>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
